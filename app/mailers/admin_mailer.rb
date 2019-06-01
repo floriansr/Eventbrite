c20f824551f0a1843051f2358afc0cdf18f81664
floriansr <sueurfloriansf@gmail.com>
2019-06-01 14:52:50 +0200

    AdminMailer configurated w/ after_create: attendance (say hello to new guest)

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,2 +1,15 @@
 class AdminMailer < ApplicationMailer
+	def say_hi_to_admin_for_new_attendance_guest(attendance)
+    #on récupère l'instance attendance pour ensuite pouvoir la passer à la view en @admin
+    @attendance = attendance
+    @event = @attendance.event
+    @admin = @event.admin
+
+
+    #on définit une variable @url qu'on utilisera dans la view d’e-mail
+    @url  = 'htttp: mon lien vers mon event' 
+
+    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
+    mail(to: @admin.email, subject: 'Un nouveau participant pour votre event !')
+  end
 end
--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,10 @@
 class Attendance < ApplicationRecord
 	belongs_to :user, :optional => true
     belongs_to :event, :optional => true
+
+    after_create :say_hi_to_admin_for_new_attendance_guest
+
+    def say_hi_to_admin_for_new_attendance_guest
+    AdminMailer.say_hi_to_admin_for_new_attendance_guest(self).deliver_now
+ 	end
 end
